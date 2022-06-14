a7347cb4f233eb18f4d8495d68fc91adb47285ac
Derian Tungka <derian.tungka@datadoghq.com>
2022-06-14 12:09:15 +0800

    update rum init to juiceshop app

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,13 +11,11 @@ import { environment } from './environments/environment'
 import { datadogRum } from "@datadog/browser-rum";
 
 datadogRum.init({
-  applicationId: "b42307fb-ff98-46d6-8e9a-775835859486",
-  clientToken: "pub32fa20c16ad226ea6d50b8170b4bf5d5",
+  applicationId: "b5d37a1f-2cd1-443d-9f94-9e1272d9c5c8",
+  clientToken: "pub9e3ca1c02e83197c770e2172316c6515",
   site: "datadoghq.com",
-  service: "derian-juiceshop",
-  env: "local",
-  // Specify a version number to identify the deployed version of your application in Datadog
-  // version: '1.0.0',
+  service: "juiceshop",
+  env: "dev",
   sampleRate: 100,
   premiumSampleRate: 100,
   trackInteractions: true,
